cb7c185662b6b434cabb4c72c37354e922ab4772
patrick-replogle <patrickr1138@gmail.com>
2020-01-13 20:50:48 -0800

    basic get request, post, and delete seem to be working. get by id not working and put request not in place.

--- index.js
+++ index.js
@@ -8,16 +8,70 @@ server.listen(4000, () => {
   console.log("=== server listening on port 4000 ===");
 });
 
+server.use(express.json());
+
 server.get("/api/users", (req, res) => {
-  res.json(db);
+  db.find()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err,
+        success: false
+      });
+    });
 });
 
 server.get("/api/users/:id", (req, res) => {
-  const user = db.find(user => user.id === req.params.id);
-
-  if (user) {
-    res.json(user);
-  } else {
-    res.status(404).json({ error: "User not found" });
-  }
+  const id = req.params.id;
+  const user = db.findById(user => user.id === req.params.id);
+  db.findById(user)
+    .then(res => {
+      if (user) {
+        res.status(200).json(res);
+      } else {
+        res.status(404).json({
+          error: "User not found"
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err,
+        success: false
+      });
+    });
 });
+
+server.post("/api/users", (req, res) => {
+  const newUser = req.body;
+  db.insert(newUser)
+    .then(user => {
+      res.status(201).json({ success: true, user });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err,
+        success: false
+      });
+    });
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  db.remove(id)
+    .then(deletedUser => {
+      if (deletedUser) {
+        res.status(204).end();
+      } else {
+        res.status(404).json({ message: `Could not find id ${id}` });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ success: false, err });
+    });
+});
+
+server.put("/api/users/:id", (req, res) => {});
