28473c0ac929bfe7bd0efdd90337e91ee657f3af
patrick-replogle <patrickr1138@gmail.com>
2020-01-13 19:51:19 -0800

    initial commit. basic server in place and listening.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+
+server.listen(4000, () => {
+  console.log("=== server listening on port 4000 ===");
+});
+
+server.get("/api/users", (req, res) => {
+  res.json(db);
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const user = db.find(user => user.id === req.params.id);
+
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).json({ error: "User not found" });
+  }
+});
